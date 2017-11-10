6d77d0c08f97c874926a89ff23ef71611de532a4
Gaël PORTAY <gael.portay@savoirfairelinux.com>
2017-11-09 22:18:08 -0500

    tests: tgz: add installation of file and dir
    Binary files and directories can be installed using $(pkgname)-bin,
    $(pkgname)-sbin and $(pkgname)-dir.

--- tests/tgz.mk
+++ tests/tgz.mk
@@ -22,6 +22,22 @@ endef
 $(foreach pkg,$(pkg-m),$(eval $(call do_pkg_run_deps,$(pkg))))
 pkg-m		:= $(sort $(pkg-m))
 
+define do_install_file =
+.SILENT: $(TGZDIR)/$(1)_$(2)/$(3)/$(notdir $(4))
+$(TGZDIR)/$(1)_$(2)/$(3)/$(notdir $(4)): $(4)
+	install -D -m 755 $$< $$@
+
+$(TGZDIR)/$(1)_$(2).tgz: $(TGZDIR)/$(1)_$(2)/$(3)/$(notdir $(4))
+endef
+
+define do_install_dir =
+.SILENT: $(TGZDIR)/$(1)_$(2)/$(3)
+$(TGZDIR)/$(1)_$(2)/$(3):
+	install -d -m 644 $$@/
+
+$(TGZDIR)/$(1)_$(2).tgz: $(TGZDIR)/$(1)_$(2)/$(3)
+endef
+
 define do_pkg_script =
 ifneq (,$($(1)-$(3)))
 .SILENT: $(TGZDIR)/$(1)_$(2)$(LOCALSTATEDIR)/info/$(1)/$(3)
@@ -52,6 +68,9 @@ $(TGZDIR)/$(1)_$(2)$(LOCALSTATEDIR)/info/$(1)/control:
 	cat $$@
 
 $(foreach script,preinst postinst prerm postrm,$(eval $(call do_pkg_script,$(1),$(2),$(script))))
+$(foreach dir,$($(1)-dir),$(eval $(call do_install_dir,$(1),$(2),$(dir))))
+$(foreach bin,$($(1)-sbin),$(eval $(call do_install_file,$(1),$(2),$(sbindir),$(bin))))
+$(foreach bin,$($(1)-bin),$(eval $(call do_install_file,$(1),$(2),$(bindir),$(bin))))
 
 tgzdir-m  += $(TGZDIR)/$(1)_$(2)
 
